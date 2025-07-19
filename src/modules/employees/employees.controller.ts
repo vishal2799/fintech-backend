ab49c4ad6b3ffd15f8f8062527ea531893cf8514
Vishal Sharma <sharma.vishal2799@gmail.com>
2025-07-19 07:03:13 +0530

    employee updated

--- src/modules/employees/employees.controller.ts
+++ src/modules/employees/employees.controller.ts
@@ -5,20 +5,20 @@ import { successHandler } from '../../utils/responseHandler';
 
 export const createEmployee = asyncHandler(async (req: Request, res: Response) => {
   const result = await EmployeeService.createEmployee(req);
-  return successHandler(res, result, 'Employee created');
+  return successHandler(res, {data: result, message: 'Employee Created successfully', status: 201});
 });
 
 export const listEmployees = asyncHandler(async (req: Request, res: Response) => {
   const result = await EmployeeService.listEmployees(req);
-  return successHandler(res, result);
+  return successHandler(res, {data: result, message: 'Employee Fetched successfully', status: 200});
 });
 
 export const updateEmployee = asyncHandler(async (req: Request, res: Response) => {
   await EmployeeService.updateEmployee(req);
-  return successHandler(res, null, 'Employee updated');
+  return successHandler(res, {data: null, message: 'Employee Updated successfully', status: 200});
 });
 
 export const deleteEmployee = asyncHandler(async (req: Request, res: Response) => {
   await EmployeeService.deleteEmployee(req);
-  return successHandler(res, null, 'Employee deleted');
+  return successHandler(res, {data: null, message: 'Employee Deleted successfully', status: 200});
 });
