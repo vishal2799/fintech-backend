3c82aca7a171e63c8fc7a88570a716953cf1625b
Vishal Sharma <sharma.vishal2799@gmail.com>
2025-08-20 20:24:22 +0530

    jwt time increased

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -8,7 +8,7 @@ const JWT_SECRET = process.env.JWT_SECRET!;
 const REFRESH_SECRET = process.env.REFRESH_SECRET!;
 
 export const generateAccessToken = (payload: any) => jwt.sign(payload, JWT_SECRET, { expiresIn: '30m' });
-export const generateRefreshToken = (payload: any) => jwt.sign(payload, REFRESH_SECRET, { expiresIn: '7d' });
+export const generateRefreshToken = (payload: any) => jwt.sign(payload, REFRESH_SECRET, { expiresIn: '1d' });
 
 export const verifyAccessToken = (token: string) => jwt.verify(token, JWT_SECRET);
 export const verifyRefreshToken = (token: string) => jwt.verify(token, REFRESH_SECRET);
